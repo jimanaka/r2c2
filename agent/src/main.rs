aa45b611253bde5ff0ac928c5bef5923d763f55b
Jake Imanaka <jake.imanaka@gmail.com>
2025-12-28 12:57:46 -1000

    initial rest api and agent connection

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use reqwest::{Client, get};
+use std::error::Error;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Tunnel {
+        #[arg(short, long, default_value_t = String::from("127.0.0.1"))]
+        ip: String,
+
+        #[arg(short, long, default_value_t = 3000)]
+        port: u16,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Tunnel { ip, port } => {
+            println!("Connecting to {}:{}", ip, port);
+            let url = format!("http://{}:{}", ip, port);
+            let body = get(&url).await?.text().await?;
+
+            println!("Responds {}", body);
+        }
+    }
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = String::from("127.0.0.1"))]
+    ip: String,
+
+    #[arg(short, long, default_value_t = 3000)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    println!("[i] Starting server...");
+
+    let args = Args::parse();
+
+    let url = format!("{}:{}", &args.ip, &args.port);
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind(&url).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
